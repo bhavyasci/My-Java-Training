3f1b55dd6b239736aba4a8899cb38a5faa5b4282
bhavyasci <bhavya.ks@somaiya.edu>
2020-05-25 13:34:48 +0530

    New files

---
+++ Sum 3 and 5 Challenge/src/Bhavya/Company/Main.java
@@ -0,0 +1,20 @@
+package Bhavya.Company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sumOf3=0;
+        int sumOf5=0;
+
+        for(int i=1;i<=1000;i++){
+           if(i%3==0 && i%5==0)
+           {
+               sumOf3=sumOf3+i;
+               sumOf5=sumOf5+i;
+           }
+           else if(i%)
+
+
+        }
+    }
+}
