8623d9685a504c429d19fb3e70c0f89ba5c14732
bhavyasci <bhavya.ks@somaiya.edu>
2020-05-29 11:42:05 +0530

    new files

--- Sum 3 and 5 Challenge/src/Bhavya/Company/Main.java
+++ Sum 3 and 5 Challenge/src/Bhavya/Company/Main.java
@@ -9,12 +9,41 @@ public class Main {
         for(int i=1;i<=1000;i++){
            if(i%3==0 && i%5==0)
            {
-               sumOf3=sumOf3+i;
-               sumOf5=sumOf5+i;
+               sumOf3=sumOf3+1;
+               sumOf5=sumOf5+1;
            }
-           else if(i%)
+           else if(i%3==0)
+               sumOf3 = sumOf3+1;
+           else if(i%5==0)
+               sumOf5= sumOf5+1;
 
 
         }
+
+        System.out.println("Numbers divided by 3 are "+sumOf3);
+        System.out.println("Numbers Divided by 5 are "+sumOf5);
+
+        int number = 4;
+        int finishNumber = 20;
+
+        while (number<=finishNumber){
+            number++;
+            if(!isEvenNumber(number)){
+                continue;
+            }
+            System.out.println("its an even number " + number);
+        }
+
+
+
     }
+
+
+    public static boolean isEvenNumber(int n){
+        if(n%2==0)
+            return true;
+        else
+            return false;
+    }
+
 }
